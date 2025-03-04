d7afaa35ae43175f60f5cf1b15fae6741ec1e5ec
Mina Ayman <mtrmena176@gmail.com>
2025-03-04 20:05:34 +0200

    create slider widget and height label ,refactor

--- lib/main.dart
+++ lib/main.dart
@@ -30,10 +30,77 @@ class BmiCalculator extends StatelessWidget {
                   gender: 'female',
                 ),
               ],
+            ),
+            Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: Container(
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    width: 2,
+                    color: const Color.fromARGB(255, 219, 219, 219),
+                  ),
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                height: 150,
+                width: double.infinity,
+                child: Column(
+                  children: [
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Text(
+                      "HEIGHT   ",
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+                    ),
+                    CustomSlider()
+                  ],
+                ),
+              ),
             )
           ],
         ),
       ),
+    );
+  }
+}
+
+class CustomSlider extends StatefulWidget {
+  const CustomSlider({
+    super.key,
+  });
+
+  @override
+  State<CustomSlider> createState() => _CustomSliderState();
+}
+
+class _CustomSliderState extends State<CustomSlider> {
+  double currentSliderPrimaryValue = 173;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        RichText(
+          text: TextSpan(style: DefaultTextStyle.of(context).style, children: [
+            TextSpan(
+              text: currentSliderPrimaryValue.toInt().toString(),
+              style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+            ),
+            TextSpan(text: 'cm')
+          ]),
+        ),
+        Slider(
+            value: currentSliderPrimaryValue,
+            min: 140,
+            max: 230,
+            activeColor: Colors.blue,
+            onChanged: (value) {
+              setState(() {
+                currentSliderPrimaryValue = value;
+              });
+            }),
+      ],
     );
   }
 }
