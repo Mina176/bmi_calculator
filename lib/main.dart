517cbe7999e4fbbd33f394efff5e1717f19a0e3e
Mina Ayman <mtrmena176@gmail.com>
2025-03-04 17:50:13 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const BmiCalculator());
+}
+
+class BmiCalculator extends StatelessWidget {
+  const BmiCalculator({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(brightness: Brightness.dark),
+      home: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text('BMI Calculator'),
+        ),
+      ),
+    );
+  }
+}
